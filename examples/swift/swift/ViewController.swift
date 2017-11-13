37393c3b3e0ca9155fd55325e15e489ee569225d
Kentarou Haneda <kentaro.haneda@rarejob.co.jp>
2017-11-13 11:07:16 +0900

    add swift project

---
+++ examples/swift/swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift
+//
+//  Created by 羽田 健太郎 on 2017/11/13.
+//  Copyright © 2017年 羽田 健太郎. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
