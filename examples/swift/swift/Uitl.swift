1083c10131bfe73be056c5cff693dd6c5d23960d
Kentarou Haneda <kentaro.haneda@rarejob.co.jp>
2017-11-13 14:35:05 +0900

    add data connection logic and views

--- examples/swift/swift/Uitl.swift
+++ examples/swift/swift/Uitl.swift
@@ -8,6 +8,7 @@
 
 import Foundation
 import UIKit
+import SkyWay
 
 extension UIViewController {
     
@@ -33,4 +34,46 @@ extension UIAlertController {
         viewController.present(alert, animated: true, completion: nil)
     }
     
+    class func showSelectPeerIdDialog(peerIds:[String], handler:@escaping (_ peerId:String)->Void){
+        let alert = self.init(title: "接続中のPeerId", message: "接続先を選択してください", preferredStyle: .alert)
+        
+        for peerId in peerIds{
+            let peerIdAction = UIAlertAction(title: peerId, style: .default, handler: { (alert) in
+                handler(peerId)
+            })
+            alert.addAction(peerIdAction)
+        }
+        
+        let viewController = UIViewController.getFrontViewController()
+        viewController.present(alert, animated: true, completion: nil)
+
+    }
+}
+
+class Util{
+    
+    class func loadConnectedPeerIds(peer:SKWPeer, myPeerId:String?, handler:@escaping (_ peerIds:[String]?)->Void){
+        peer.listAllPeers({ (peers) -> Void in
+            if let connectedPeerIds = peers as? [String]{
+                let res = connectedPeerIds.filter({ (connectedPeerId) -> Bool in
+                    return connectedPeerId != myPeerId
+                })
+                handler(res)
+            }else{
+                handler(nil)
+            }
+        })
+    }
+    
+    class func callPeerIDSelectDialog(peer:SKWPeer, myPeerId:String?, handler:@escaping (_ selectedPeerId:String)->Void){
+        self.loadConnectedPeerIds(peer: peer, myPeerId: myPeerId) { (peerIds) in
+            if let _peerIds = peerIds, _peerIds.count > 0{
+                UIAlertController.showSelectPeerIdDialog(peerIds: _peerIds, handler: { (peerId) in
+                    handler(peerId)
+                })
+            }else{
+                UIAlertController.oneButton("確認", message: "接続中の他のPeerIdはありません", handler: nil)
+            }
+        }
+    }
 }
