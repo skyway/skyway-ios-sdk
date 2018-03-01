e7e5b07e8be38ffa570930879dacb0b2c06d0628
Tsuyoshi TAKAHASHI <tsu.takahashi@SR5-takahashi-mbp.local>
2018-03-01 17:04:40 +0900

    copy p2p-videochat to p2p-call and rename package

---
+++ examples/p2p-call/eclwebrtc-ios-sample-p2p-call/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  eclwebrtc-ios-sample-p2p-call
+//
+//  Created by NTT Communications on 2018/03/01.
+//  Copyright © 2018年 NTT Communications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <SkyWay/SKWPeer.h>
+#import "PeerListViewController.h"
+
+@interface ViewController : UIViewController <PeerListDelegate>
+
+
+@end
+
---
+++ examples/p2p-call/eclwebrtc-ios-sample-p2p-call/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  eclwebrtc-ios-sample-p2p-call
+//
+//  Created by NTT Communications on 2018/03/01.
+//  Copyright © 2018年 NTT Communications. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
